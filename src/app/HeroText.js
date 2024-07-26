02c9f08ed1c39e2f8e71622505984e34f6a1ca86
Sanish <sanishpagui162@gmail.com>
2024-07-26 19:01:03 +0530

    ez

--- src/app/HeroText.js
+++ src/app/HeroText.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HeroText = () => {
   return (
-    <div className="w-full h-[20%] sm:text-6xl sm:h-[40%] text-4xl md:text-6xl flex flex-col md:h-[25%] tracking-tighter xl:text-8xl xl:h-[45%] lg:pl-12 lg:text-7xl lg:h-[35%] justify-center pl-4 md:pl-6">
-      <span className=" font-semibold mt-6 sm:mt-20 md:mt-4 lg:mt-16">Design studio</span>
+    <div className="w-full h-[25%] sm:text-6xl sm:h-[40%] text-4xl md:text-6xl flex flex-col md:h-[25%] tracking-tighter xl:text-8xl xl:h-[45%] lg:pl-12 lg:text-7xl lg:h-[35%] justify-center pl-3 md:pl-6">
+      <span className=" font-semibold mt-10 sm:mt-20 md:mt-4 lg:mt-16">Design studio</span>
       <span className=" font-semibold">for timeless</span>
       <span className=" font-semibold">branding</span>
     </div>
