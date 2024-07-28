16afc382be2d7d97e4cf0b0bd8a26c612b4d4a94
Sanish <sanishpagui162@gmail.com>
2024-07-28 18:25:51 +0530

    ez

--- src/app/MainVideo.js
+++ src/app/MainVideo.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 
 const MainVideo = () => {
   return (
-    <div className='mt-4 w-full'>
+    <div className='mt-4 w-full bg-black'>
         <video className="w-full video video-preview on-scroll video-desktop" poster="https://studio-size.com/wp-content/uploads/2024/06/Reel-Preview-2.gif" muted="" loop="" playsinline="" src="https://player.vimeo.com/progressive_redirect/playback/953957232/rendition/1080p/file.mp4?loc=external&amp;log_user=0&amp;signature=83883f7f7a50c6aeb64e3b1b1f466615177dae78f2295dc7a57d51752c5b712a" preload="metadata"></video>
         <h1 className='absolute z-10 md:hidden -translate-y-20  pl-4 text-xl sm:text-2xl font-extrabold'>Showreel</h1>
         <h2 className='absolute z-10 md:hidden -translate-y-12 text-gray-500 sm:text-2xl text-xl pl-4'>45sec</h2>
--- src/app/component/FeaturedWorks.js
+++ src/app/component/FeaturedWorks.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const FeaturedWorks = () => {
   return (
-    <div className=' w-screen bg-black'>
-        <div className='mt-14 px-2 md:px-6 flex justify-between items-center'>
+    <div className=' w-full bg-black'>
+        <div className='pt-28 md:pt-14 px-2 md:px-6 flex justify-between items-center'>
                 <h1 className='text-[8vw] font-semibold md:text-[4vw]'>Featured Work</h1>
                 <button className='text-[5vw] w-[25vw] h-[14vw] border-[1px] md:w-[10vw] md:h-[5vh] md:text-[2vw] border-[#1D1D1D] rounded-full'>View All</button>
         </div>
