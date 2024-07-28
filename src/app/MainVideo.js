07528f7f1343791b64737a6935a88663ef57bcfd
Sanish <sanishpagui162@gmail.com>
2024-07-28 11:50:25 +0530

    ez

--- src/app/MainVideo.js
+++ src/app/MainVideo.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+
 const MainVideo = () => {
   return (
     <div className='mt-2 w-full'>
         <video className="w-screen" poster="https:/studio-size.com/wp-content/uploads/2024/06/Reel-Preview-2.gif"></video>
-        <div className='absolute z-10 '>
-        <h1 className='-translate-y-20  pl-4 text-xl font-extrabold'>Showreel</h1>
-        <h2 className='-translate-y-20 text-gray-500 text-xl pl-4'>45sec</h2>
+        <div className='absolute z-10 md:hidden'>
+        <h1 className='-translate-y-20  pl-4 text-xl sm:text-2xl font-extrabold'>Showreel</h1>
+        <h2 className='-translate-y-20 text-gray-500 sm:text-2xl text-xl pl-4'>45sec</h2>
         </div>
     </div>
   )
