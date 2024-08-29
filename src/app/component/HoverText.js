aba7fca1e6a4f18fcfac3eca2c722bb67d912cb5
Sanish <sanishpagui162@gmail.com>
2024-08-29 21:11:02 +0530

    ez

--- src/app/component/HoverText.js
+++ src/app/component/HoverText.js
@@ -2,12 +2,26 @@ import React from 'react'
 
 const HoverText = () => {
   return (
-    <div className='bg-black h-screen w-full border-t-2 border-black'>
+    <div className='bg-black h-fit w-full border-t-2 border-black'>
         <div className='pl-2 mt-[20vw] '>
             <div>Services</div>
-            <div className='pl-1 text-[10vw] mt-[4vw]'>
-                <div className=''>
+            <div className='pl-1 text-[10vw] md:text-[5vw] md:leading-[6vw] mt-[4vw] leading-[12vw] md:flex '>
+                <div className='w-1/2'>
+
+                </div>
+                <div className='md:cursor-pointer md:hover:text-white'>
                     <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Branding</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Brand Strategy</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Motion graphics</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Video editing</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>3D animation</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Audio production</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Web design</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Photography</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Naming</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Packaging</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Advertising</div>
+                    <div className='text-[#3b3b3b] font-extrabold tracking-tighter'>Brand art</div>
                 </div>
             </div>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React,{useEffect} from 'react'
 import MenuComponent from './component/MenuComponent'
 import HeroText from '@app/HeroText'
 import MainVideo from './component/MainVideo'
@@ -7,17 +7,42 @@ import FeaturedWorks from './component/FeaturedWorks'
 import Cards from './component/Cards'
 import AfterCard from './component/AfterCard'
 import HoverText from './component/HoverText'
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+import { ReactLenis, useLenis } from 'lenis/react'
+import Lenis from '@studio-freight/lenis'
+gsap.registerPlugin(ScrollTrigger)
 
 export default function Page() {
+  useEffect(()=>{
+    const lenis=new Lenis({
+      duration: 2,
+      easing:(t)=>Math.min(1,1.001-Math.pow(2,-10*t)),
+      smooth:true,
+      direction:'vertical',
+      gestureDirection:'vertical',
+      smoothTouch: false,
+    })
+    function raf(time){
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+    return()=>{
+      lenis.destroy()
+    }
+  })
   return (
-    <div className="hero w-auto bg-black text-white h-dvh">
-      <MenuComponent/>
-      <HeroText/>
-      <MainVideo/>
-      <FeaturedWorks/>
-      <Cards/>
-      <AfterCard/>
-      <HoverText/>
+    <div id='smooth-wrapper' className="hero w-auto bg-black text-white h-dvh">
+      <div id='smooth-content'>
+        <MenuComponent/>
+        <HeroText/>
+        <MainVideo/>
+        <FeaturedWorks/>
+        <Cards/>
+        <AfterCard/>
+        <HoverText/>
+      </div>
     </div>
   )
 }
